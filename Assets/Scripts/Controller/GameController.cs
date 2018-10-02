5733b694bdb46755671a7cc45e2ed7f217b7370d
Rick Rijkers <Rick.RIjkers1@gmail.com>
2018-10-02 10:51:30 +0200

    removed standard assets and added my own charachterController

---
+++ Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : Singleton<GameController>
+{
+    public CharacterController player;
+    public int score;
+    public GameObject playerDiedWindow;
+    // Use this for initialization
+    void Start()
+    {
+        playerDiedWindow.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.dead)
+        {
+            playerDiedWindow.SetActive(true);
+            if (InputManager.Instance.restartButton)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+        if(InputManager.Instance.escButton){
+            Application.Quit();
+        }
+    }
+}
