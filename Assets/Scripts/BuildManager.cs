afdf8e6b6bd58cc5b0a8789acc8de5b28880c3b5
MICHIEL-LT\michiel <michielhendrix@hotmail.com>
2018-10-03 21:20:43 +0200

    camera controles + placing characters
    you can place characters and you can move over the map whit you mouse

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    public GameObject standardCharacterPrefab;
+
+    private GameObject characterToBuild;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("there are way to many");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        characterToBuild = standardCharacterPrefab;
+    }
+
+    public GameObject GetCharacterToBuild()
+    {
+        return characterToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+
+    public GameObject character;
+
+    private Renderer rend;
+    private Color startColor;
+
+    public void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if(character != null)
+        {
+            // diplay later on as a text
+            Debug.Log("cant place a character here");
+            return;
+        }
+
+        GameObject charcaterToBuild = BuildManager.instance.GetCharacterToBuild();
+
+        character = (GameObject)Instantiate(charcaterToBuild, transform.position, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
