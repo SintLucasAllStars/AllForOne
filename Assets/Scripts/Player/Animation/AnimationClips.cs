95e2cbf4fe60c2965e4fb6f593b844805f8e1986
bt2 <koengroten@gmail.com>
2018-10-05 19:23:59 +0200

    fixed lfs siteaution and player controller

--- Assets/Scripts/Player/Animation/AnimationClips.cs
+++ Assets/Scripts/Player/Animation/AnimationClips.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class AnimationClips : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
