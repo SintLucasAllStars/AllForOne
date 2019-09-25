e7d1dd65d6f5c93913716fc07d658d2cd2b2a8c3
Juul_Cox <juulcox00@gmail.com>
2019-09-25 15:25:39 +0200

    created actor and weapon class.
    added asset pack for sci fi coridors

---
+++ AllForOne/Assets/Actor.cs
@@ -0,0 +1,27 @@
+﻿public class Actor
+{
+    public int health;
+    public int speed;
+    public int strenght;
+    public int defense;
+
+    public Actor(int health, int speed, int strenght, int defense)
+    {
+        this.health = health;
+        this.speed = speed;
+        this.defense = defense;
+        this.strenght =strenght;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public void Heal(int healAmount)
+    {
+        health += healAmount;
+    }
+
+ 
+}
