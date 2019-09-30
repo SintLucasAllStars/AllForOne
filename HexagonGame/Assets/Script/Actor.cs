f8c08521373034cf9e2a199d89f6b307c4dbccf8
Stan Vogels <stanvogels@live.nl>
2019-09-30 12:27:03 +0200

    added warrior

---
+++ HexagonGame/Assets/Script/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+
+    private Warrior warrior;
+
+    public void Start()
+    {
+        warrior = new Warrior();
+    }
+
+
+}
---
+++ HexagonGame/Assets/Script/Warrior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior
+{
+    private int health;
+    private int strength;
+    private int speed;
+    private int defense;
+    
+    public Warrior()
+    {
+        this.health = 5;
+        this.strength = 5;
+        this.speed = 5;
+        this.defense = 5;
+    }
+
+    public Warrior(int a_Health, int a_Strenth, int a_Speed, int a_Defense)
+    {
+        this.health = a_Health;
+        this.strength = a_Strenth;
+        this.speed = a_Speed;
+        this.defense = a_Defense;
+    }
+
+    public void SetHealth(int a_Health) { this.health = a_Health; }
+    public int GetHealth() { return this.health; }
+
+    public void SetStrength(int a_Strength) { this.strength = a_Strength; }
+    public int GetStrenth() { return this.strength; }
+
+    public void SetSpeed(int a_Speed) { this.speed = a_Speed; }
+    public int GetSpeed() { return this.speed; }
+
+    public void SetDefense(int a_Defence) { this.defense = a_Defence; }
+    public int GetDefense() { return this.defense; }
+}
