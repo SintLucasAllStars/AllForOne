d0157ac8164eacc1258c477b8d95a68c4675f3ed
Cis Donkers <c.donkers@sintlucasedu.nl>
2019-10-02 16:20:35 +0200

    ✔ Added unit placement mechanic

---
+++ Assets/Scripts/SliderValue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class SliderValue : MonoBehaviour
+{
+    public Slider sliderUI;
+    private TMP_Text textSliderValue;
+
+    void Start()
+    {
+        textSliderValue = GetComponent<TMP_Text>();
+        ShowHealthValue();
+    }
+
+    public void ShowHealthValue()
+    {
+        string sliderMessage = sliderUI.value.ToString();
+        textSliderValue.text = sliderMessage;
+    }
+}
---
+++ Assets/Scripts/UnitPlacer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPlacer : MonoBehaviour
+{
+
+    public GameObject unitP1, unitP2;
+    public GameObject UnitListP1, UnitListP2;
+
+    public bool switchTurn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+ {
+            switchTurn = !switchTurn;
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            if(switchTurn == false)
+            {
+                GameObject newUnit = Instantiate(unitP1, hit.point, Quaternion.identity);
+                newUnit.transform.parent = UnitListP1.transform;
+            }
+            else
+            {
+                GameObject newUnit = Instantiate(unitP2, hit.point, Quaternion.identity);
+                newUnit.transform.parent = UnitListP2.transform;
+            }
+        }
+    }
+}
