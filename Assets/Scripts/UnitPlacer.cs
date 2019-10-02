b3ef70edec630e9031b90c0fd6583e5d1e03e923
pieterdebot <pieter.de.bot@merapar.com>
2019-10-02 09:51:15 +0200

    Added UnitPlacer
    This system will allow you to configure the unit stats.

---
+++ Assets/Scripts/UnitPlacer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitPlacer : MonoBehaviour
+{
+    public Text unitPrice;
+    public Slider healthSlider, strengthSlider, speedSlider, defenseSlider;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        unitPrice.text = CalculatePrice() + "";
+    }
+
+    private int CalculatePrice()
+    {
+        return (int) healthSlider.value + (int) strengthSlider.value + (int) speedSlider.value + (int) defenseSlider.value;
+    }
+    
+}
