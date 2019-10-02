cbb4d47cac9d9710b02aa52c9f798ddc83a29dc9
mikedriessen <mlh.driessen@live.nl>
2019-10-02 12:54:27 +0200

    Added UnitPlacement

--- Assets/_Scripts/UnitPlacement.cs
+++ Assets/_Scripts/UnitPlacement.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UnitPlacement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text points, cost;
+    public Slider healthSlider, strengthSlider, speedSlider, defenceSlider;
+    public Image background;
+    public Button ready, place;
+    
+    //Disable PlayerController
+    public GameObject [] playerObj;
+ //Select player 1 or 2;
+    private int player;
+    private int currentPoints, currentCost;
+    
+    //Player ready
+    private bool canStart;
+    
+ //   public GameManager unitPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ private void Awake()
+ {
+     //SetValues
+     currentPoints = 100;
+     currentCost = 0;
+     
+     //Start
+     Statup();
+     
+ }
+
+ private void Statup()
+ {
+     DisableOther();
+     
+     DisplaySliders();
+     DisplayButtons();
+     DisplayBackground();
+     setValues(currentPoints, currentCost);
+
+ }
+
+ private void DisableOther()
+ {
+     playerObj = GameObject.FindGameObjectsWithTag("Player");
+     playerObj[0].gameObject.SetActive(false);
+ }
+
+ private void setValues(int points, int cost)
+ {
+     this.cost.text = cost+ "";
+     this.points.text = points + "";
+ }
+
+ private void DisplaySliders()
+ {
+     healthSlider.gameObject.SetActive(true);
+     strengthSlider.gameObject.SetActive(true);
+     speedSlider.gameObject.SetActive(true);
+     defenceSlider.gameObject.SetActive(true);
+ }
+
+ private void DisplayButtons()
+ {
+     ready.gameObject.SetActive(true);
+     place.gameObject.SetActive(true);
+ }
+
+ private void DisplayBackground()
+ {
+     background.gameObject.SetActive(true);
+ }
 }
