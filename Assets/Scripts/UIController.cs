71d7288ce701e88b1f43c4d7eb53b428130612ad
ertugrul013 <ertugrul013@hotmail.com>
2019-10-03 15:45:36 +0200

    :sparkles: Added the basic code for the charachter buy screen
    the function will handle the translation between the sliders and script

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private Slider damage_s, speed_s, range_s;
 
-public class UIController : MonoBehaviour {
+    private int damage_i, speed_i, range_i;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void CharacterUI()
+    {
+        damage_s.value = damage_i;
+        speed_s.value = speed_i;
+        range_s.value = range_i;
+        GameController.instance.BuyUnit(damage_i, speed_i, range_i);
+    }
 }
