b20930fff5b5182c703d4c018b7f6da725018d32
m.eind <36227686+4561351658456@users.noreply.github.com>
2019-10-08 20:40:32 +0200

    Add camera movement, gamemanager and progress level design

---
+++ AllForOne/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private float _minClampX;
+
+    [SerializeField]
+    private float _maxClampX;
+
+    [SerializeField]
+    private float _zoomSpeed;
+
+    [SerializeField]
+    private float _minZoom;
+
+    [SerializeField]
+    private float _maxZoom;
+
+    private float _xRotation;
+    private float _yRotation;
+
+    private void Update()
+    {
+        CameraMovement();
+        CameraZoom();
+    }
+
+    private void CameraMovement()
+    {
+        float xInput = Input.GetAxis("Vertical");
+        float yInput = Input.GetAxis("Horizontal");
+
+        _xRotation += xInput;
+        _yRotation += yInput;
+
+        _xRotation = Mathf.Clamp(_xRotation, _minClampX, _maxClampX);
+
+        transform.eulerAngles = new Vector3(_xRotation, -_yRotation, 0);
+    }
+
+    private void CameraZoom()
+    {
+        if (Input.GetKey(KeyCode.Minus) && _camera.fieldOfView >= _minZoom)
+        {
+            _camera.fieldOfView -= _zoomSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Equals) && _camera.fieldOfView <= _maxZoom)
+        {
+            _camera.fieldOfView += _zoomSpeed;
+        }
+    }
+}
---
+++ AllForOne/Assets/Scripts/GameManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int _health = 1;
+    private int _strenght = 1;
+    private int _speed = 1;
+    private int _defense = 1;
+
+    private int _expensiveCostModifier = 3;
+    private int _cheapCostModifier = 2;
+
+    private int _player1Points = 100;
+    private int _player2Points = 100;
+
+    private bool _player1Turn = true;
+
+    public void AddHealthPoints(int health)
+    {
+        if ((_health == 10 && health > 0) || (_health == 1 && health < 0))
+        {
+            return;
+        }
+
+        if (_player1Turn)
+        {
+            _player1Points -= health * _expensiveCostModifier;
+            if (_player1Points < 0)
+            {
+                _player1Points += health * _expensiveCostModifier;
+                return;
+            }
+        }
+        else
+        {
+            _player2Points -= health * _expensiveCostModifier;
+            if (_player2Points < 0)
+            {
+                _player2Points += health * _expensiveCostModifier;
+                return;
+            }
+        }
+
+        _health += health;
+    }
+
+    public void AddStrenghtPoints(int strenght)
+    {
+        if ((_strenght == 10 && strenght > 0) || (_strenght == 1 && strenght < 0))
+        {
+            return;
+        }
+
+        if (_player1Turn)
+        {
+            _player1Points -= strenght * _cheapCostModifier;
+            if (_player1Points < 0)
+            {
+                _player1Points += strenght * _cheapCostModifier;
+                return;
+            }
+        }
+        else
+        {
+            _player2Points -= strenght * _cheapCostModifier;
+            if (_player2Points < 0)
+            {
+                _player2Points += strenght * _cheapCostModifier;
+                return;
+            }
+        }
+
+        _strenght += strenght;
+    }
+
+    public void AddSpeedPoints(int speed)
+    {
+        if ((_speed == 10 && speed > 0) || (_speed == 1 && speed < 0))
+        {
+            return;
+        }
+
+        if (_player1Turn)
+        {
+            _player1Points -= speed * _expensiveCostModifier;
+            if (_player1Points < 0)
+            {
+                _player1Points += speed * _expensiveCostModifier;
+                return;
+            }
+        }
+        else
+        {
+            _player2Points -= speed * _expensiveCostModifier;
+            if (_player2Points < 0)
+            {
+                _player2Points += speed * _expensiveCostModifier;
+                return;
+            }
+        }
+
+        _speed += speed;
+    }
+
+    public void AddDefensePoints(int defense)
+    {
+        if ((_defense == 10 && defense > 0) || (_defense == 1 && defense < 0))
+        {
+            return;
+        }
+
+        if (_player1Turn)
+        {
+            _player1Points -= defense * _cheapCostModifier;
+            if (_player1Points < 0)
+            {
+                _player1Points += defense * _cheapCostModifier;
+                return;
+            }
+        }
+        else
+        {
+            _player2Points -= defense * _cheapCostModifier;
+            if (_player2Points < 0)
+            {
+                _player2Points += defense * _cheapCostModifier;
+                return;
+            }
+        }
+
+        _defense += defense;
+    }
+}
