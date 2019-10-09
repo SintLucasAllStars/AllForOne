32f7005e4c493eafbefc61fb66fe3bace4fc5c70
Cis Donkers <c.donkers@sintlucasedu.nl>
2019-10-09 14:36:02 +0200

    ✔ Updated sliderValue script

--- Assets/Scripts/SliderValue.cs
+++ Assets/Scripts/SliderValue.cs
@@ -5,36 +5,50 @@ using UnityEngine.UI;
 using TMPro;
 public class SliderValue : MonoBehaviour
 {
-    public Slider sliderUI;
-    private TMP_Text textSliderValue;
+    public Slider[] sliderUI;
+    public TMP_Text[] textSliderValue;
     public TMP_Text textTotalPoints;
 
-    public int sliderValue;
+    public int[] sliderValues;
     public int totalPointsValue = 100;
 
-    void Start()
-    {
-        textSliderValue = GetComponent<TMP_Text>();
-        //ShowValue();
-    }
+    public Button next;
 
     void Update()
     {
-
-    }
-
-    public void ShowValue()
-    {
-        string sliderMessage = sliderUI.value.ToString();
-        textSliderValue.text = sliderMessage;
-        sliderValue = int.Parse(textSliderValue.text);
-
-        totalPointsValue -= sliderValue;
+        totalPointsValue = 0;
+        for (int i = 0; i < sliderValues.Length; i++)
+        {
+            totalPointsValue -= sliderValues[i];
+            //totalPointsValue = (-sliderValues[i] * 2 + 100);
+        }
+        totalPointsValue += 100;
         textTotalPoints.text = totalPointsValue.ToString();
     }
 
-    public void ShowTotalPoints()
+    public void ShowValue1()
     {
-        
+        string sliderMessage0 = sliderUI[0].value.ToString();
+        textSliderValue[0].text = sliderMessage0;
+        sliderValues[0] = int.Parse(textSliderValue[0].text);
+    }
+
+    public void ShowValue2()
+    {
+        string sliderMessage1 = sliderUI[1].value.ToString();
+        textSliderValue[1].text = sliderMessage1;
+        sliderValues[1] = int.Parse(textSliderValue[1].text);
+    }
+    public void ShowValue3()
+    {
+        string sliderMessage2 = sliderUI[2].value.ToString();
+        textSliderValue[2].text = sliderMessage2;
+        sliderValues[2] = int.Parse(textSliderValue[2].text);
+    }
+    public void ShowValue4()
+    {
+        string sliderMessage3 = sliderUI[3].value.ToString();
+        textSliderValue[3].text = sliderMessage3;
+        sliderValues[3] = int.Parse(textSliderValue[3].text);
     }
 }
---
+++ Assets/Scripts/TestSliderValue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class TestSliderValue : MonoBehaviour
+{
+    public Slider sliderUI;
+    public TMP_Text textSliderValue;
+    public TMP_Text textTotalPoints;
+
+    public int sliderValue;
+    public int totalPointsValue = 100;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        totalPointsValue = (-sliderValue * 3 + 100);
+        textTotalPoints.text = totalPointsValue.ToString();
+    }
+
+    public void ShowValue()
+    {
+        string sliderMessage = sliderUI.value.ToString();
+        textSliderValue.text = sliderMessage;
+        sliderValue = int.Parse(textSliderValue.text);
+
+        totalPointsValue = (-sliderValue *2 +100);
+        textTotalPoints.text = totalPointsValue.ToString();
+    }
+
+    public void ShowTotalPoints()
+    {
+
+    }
+}
