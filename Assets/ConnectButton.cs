6f479bb53b971facad5064e732f556e7e00853de
TriggerWinkle <gameappartement.gastrol@gmail.com>
2019-10-20 20:07:20 +0200

    Add working notification manager

--- Assets/ConnectButton.cs
+++ Assets/ConnectButton.cs
@@ -7,24 +7,9 @@ namespace AllForOne
         public override void Press()
         {
             NetworkManager.ConnectionSuccessful += Debugs;
-            NetworkManager.ConnectionFailed += Debugs2;
             NetworkManager.Instance.Connect();
         }
 
-        private void Debugs()
-        {
-            Debug.Log("Connected");
-        }
-
-        private void Debugs2(string reason)
-        {
-            Debug.Log("Not Connected: " + reason);
-        }
-
-        void OnDisable()
-        {
-            NetworkManager.ConnectionSuccessful -= Debugs;
-            NetworkManager.ConnectionFailed -= Debugs2;
-        }
+        private void Debugs() => SceneManager.Instance.LoadScene(1);
     }
 }
---
+++ Assets/Notifier.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace AllForOne
+{
+    public class Notifier : Singleton<Notifier>
+    {
+        private Animator _animator;
+
+        [SerializeField]
+        private TextMeshProUGUI _notificationText;
+
+        private void Awake()
+        {
+            _animator = GetComponentInChildren<Animator>();
+        }
+
+        public void ShowNotification(string text)
+        {
+            _notificationText.text = text;
+            OpenModal(true);
+        }
+
+        public void OpenModal(bool isEnabled) => _animator.SetBool("isEnabled", isEnabled);
+
+        private void OnEnable()
+        {
+            NetworkManager.ConnectionFailed += ShowNotification;
+            NetworkManager.Disconnect += ShowNotification;
+        }
+
+        private void OnDisable()
+        {
+            NetworkManager.ConnectionFailed -= ShowNotification;
+            NetworkManager.Disconnect -= ShowNotification;
+        }
+    }
+}
