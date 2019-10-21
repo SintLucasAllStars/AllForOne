3dd851b5621997c0b96e3405788c960a14ee5343
Mneijndorff <33960298+Mneijndorff99@users.noreply.github.com>
2019-10-21 19:15:07 +0100

    prefab changes and added pickupable weapons

---
+++ AllForOne/Assets/!Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    public AudioSource aud;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+        aud.Play();
+    }
+}
