0f8beb6096a30ea21d2c09da3099d1d113745be4
Cis Donkers <c.donkers@sintlucasedu.nl>
2019-10-21 19:46:11 +0200

    ✔ Import powerups and weapons & spawning system

---
+++ Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    public GameObject[] powerUps;
+    public GameObject[] weapons;
+
+    public GameObject[] itemSpawns;
+
+    void Awake()
+    {
+        GameObject powerUp = powerUps[Random.Range(0, 2)];
+        GameObject weapon = weapons[Random.Range(0, 2)];
+        Instantiate(powerUp, itemSpawns[Random.Range(0,6)].transform.position, powerUp.transform.rotation);
+        Instantiate(weapon, itemSpawns[Random.Range(0, 6)].transform.position, weapon.transform.rotation);
+    }
+}
