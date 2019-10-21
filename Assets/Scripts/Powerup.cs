aa35d0c026c366963ba4cecb0e143882b8846084
TriggerWinkle <gameappartement.gastrol@gmail.com>
2019-10-21 21:10:35 +0200

    Cleaned up asset database

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace MechanicFever
+{
+    public class Powerup : MonoBehaviour
+    {
+        [SerializeField]
+        private PowerupType _powerupType;
+        public PowerupType PowerupType => _powerupType;
+
+        private void Awake() => _powerupType = (PowerupType)Random.Range(0, (int)System.Enum.GetNames(typeof(PowerupType)).Length);
+    }
+
+    public enum PowerupType
+    {
+        Adrenaline,
+        Rage,
+        TimeMachine
+    }
+}
