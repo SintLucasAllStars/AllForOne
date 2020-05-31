6586b4aee2e27ecbfaab50eed62e83aa340895f2
Ad Bressers <gamerad94@gmail.com>
2020-05-31 17:32:25 +0200

    Add Unit
    -Add Unit base script
    -Add player base script
    Working on unit&player interaction

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool m_IsControllingUnit;
+    private Vector3 movedir;
+    [SerializeField]
+    private Unit curUnit;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        curUnit.SetUp(15, 10, 10, 15, Team.Red);
+    }
+
+    private void Update()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        movedir = new Vector3(horizontalMovement, 0, verticalMovement).normalized;
+        curUnit.Move(movedir);
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = curUnit.GetCameraPos();
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform camTransform;
+    private Vector3 offset = new Vector3(0, 0, -5);
+
+    private int m_Health;
+    private int m_Strength;
+    private int m_Defence;
+    private int m_Speed;
+    private Team m_Team;
+
+    private Vector3 unitPos;
+
+    public void SetUp(int health, int strength, int defence, int speed, Team team)
+    {
+        m_Health = health;
+        m_Strength = strength;
+        m_Defence = defence;
+        m_Speed = speed;
+        m_Team = team;
+        unitPos = transform.position;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        camTransform = GetComponentInChildren<Transform>();
+    }
+
+    public void Move(Vector3 moveDir)
+    {
+        unitPos += moveDir * (m_Speed * Time.deltaTime);
+        unitPos.y = transform.position.y;
+        rb.MovePosition(unitPos);
+    }
+
+    public Vector3 GetCameraPos()
+    {
+        Vector3 transform = camTransform.position + offset;
+        return transform;
+    }
+
+    public void Look(Vector3 rotation)
+    {
+
+    }
+}
+
+public enum Team { Red, Blue }
