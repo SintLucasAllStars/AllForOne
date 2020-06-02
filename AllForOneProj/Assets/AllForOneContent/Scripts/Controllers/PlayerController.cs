cc48da59949b4c5d764ea7e5960ac2e73eee1d2c
Quinn McGregor <quinn.mcgregor@hotmail.com>
2020-06-02 11:48:37 +0200

    Init Project

---
+++ AllForOneProj/Assets/AllForOneContent/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Character possessedCharacter;
+
+
+    public void Possess(Character character)
+    {
+        if (possessedCharacter != null)
+        {
+            UnPossess();
+        }
+        possessedCharacter = character;
+        possessedCharacter.isPossessed = true;
+    }
+
+    public void UnPossess()
+    {
+        possessedCharacter.isPossessed = false;
+        possessedCharacter = null;
+    }
+
+}
