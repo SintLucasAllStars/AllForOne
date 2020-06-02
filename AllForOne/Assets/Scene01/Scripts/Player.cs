9dd0c86b27bfd4eea488fbce5d388e56fb85bb0f
KKEV1NN <44778147+KKEV1NN@users.noreply.github.com>
2020-06-02 12:01:12 +0200

    :tada: Initial Commit

---
+++ AllForOne/Assets/Scene01/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //Camera
+    public float mouseSens = 10;
+    public Transform target;
+    public float DistanceFormTarget = 2; 
+    float x, y;
+
+    enum PlayerState {None, Selected, Controlling }; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ThirdPersonCamera()
+    {
+        x += Input.GetAxisRaw("Mouse X") * mouseSens;
+        y += Input.GetAxisRaw("Mouse Y") * mouseSens;
+        y = Mathf.Clamp(y, -40, 85);
+
+        Vector3 targetRot = new Vector3(y, x);
+        transform.eulerAngles = targetRot;
+
+        transform.position = target.position - transform.position * DistanceFormTarget; 
+
+
+    }
+}
---
+++ AllForOne/Assets/Scene01/Scripts/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class Unit
+{
+    int Health;
+    int Strenght;
+    float Speed;
+    int Defense;
+
+    Vector3 curPos; 
+
+    public Unit(int a_Health, int a_Strenght, float a_Speed, int a_Defense, Vector3 a_curPos)
+    {
+        this.Health = a_Health;
+        this.Strenght = a_Strenght;
+        this.Speed = a_Speed;
+        this.Defense = a_Defense;
+        this.curPos = a_curPos;
+    }
+
+}
