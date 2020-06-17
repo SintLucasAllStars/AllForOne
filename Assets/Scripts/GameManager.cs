ddaafa4be791fff1e2c55d98e698e7428f902636
VengarvdWal <vengar_live@hotmail.com>
2020-06-17 19:14:44 +0200

    Commit One
    Added Stat Sliders, Highlight unit clicked.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : Singleton<GameManager>
+{
+    public Dictionary<GameObject, Units> units = new Dictionary<GameObject, Units>();
+
+
+    public TextMeshProUGUI m_Text;
+
+    public Slider m_HealthSlider, m_StrengthSlider, m_SpeedSlider, m_DefenseSlider;
+    public TextMeshProUGUI m_HealthText, m_StrengthText, m_SpeedText, m_DefenseText;
+
+    public GameObject cube;
+
+    private int m_PlayerCurrency = 100;
+    private int m_Player2Currency = 100;
+    private Camera m_Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = Camera.main;
+
+        //int x = 5;
+        //int z = 5;
+        //for (int i = 0; i < x; i++)
+        //{
+        //    for (int a = 0; a < z; a++)
+        //    {
+                
+                
+                
+        //    }
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+        m_HealthText.text = string.Format("Health : {0}", m_HealthSlider.value);
+        m_StrengthText.text = string.Format("Strength : {0}", m_StrengthSlider.value);
+        m_SpeedText.text = string.Format("Speed : {0}", m_SpeedSlider.value);
+        m_DefenseText.text = string.Format("Defence : {0}", m_DefenseSlider.value);
+        //do
+        //{
+
+        //} while (m_PlayerCurrency > 0 || m_Player2Currency > 0);
+
+    }
+
+    public void CreateCharacter()
+    {
+        Units unit = new Units(m_HealthSlider.value, m_StrengthSlider.value, m_SpeedSlider.value, m_DefenseSlider.value);
+        GameObject go = Instantiate(cube, new Vector3(0, 0, 0), Quaternion.identity);
+        
+        
+        Debug.LogWarning(string.Format("A Unit hase been instantiated, with {0} {1} {2} {3} that cost {4}", 
+            unit.getHealth(), unit.getStrength(), unit.getSpeed(), unit.getDefense(), unit.getPrice()));
+        units.Add(go, unit);
+    }
+    
+}
---
+++ Assets/Scripts/Units.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Principal;
+using UnityEngine;
+
+public class Units
+{
+    //Health
+    private float m_Health;
+    //Strength
+    private float m_Strength;
+    //Speed
+    private float m_Speed;
+    //Defense 
+    private float m_Defense;
+    private float m_Price;
+
+    public GameObject prefab;
+
+
+    public float getHealth()
+    {
+        return m_Health;
+    }
+
+    public GameObject getPrefab()
+    {
+        return prefab;
+    }
+
+    public float getStrength()
+    {
+        return m_Strength;
+    }
+
+    public float getSpeed()
+    {
+        return m_Speed;
+    }
+
+    public float getDefense()
+    {
+        return m_Defense;
+    }
+
+    public float getPrice()
+    {
+        return m_Price;
+    }
+
+    public Units()
+    {
+        this.m_Health = Random.Range(1, 10);
+        this.m_Strength = Random.Range(1, 10);
+        this.m_Speed = Random.Range(1, 10);
+        this.m_Defense = Random.Range(1, 10);
+        caculatePrice();
+    }
+
+    public Units(float a_Health, float a_Strength, float a_Speed, float a_Defense)
+    {
+        this.m_Health = a_Health;
+        this.m_Strength = a_Strength;
+        this.m_Speed = a_Speed;
+        this.m_Defense = a_Defense;
+        caculatePrice();
+    }
+
+    public void caculatePrice()
+    {
+        float price = m_Health + m_Strength + m_Speed + m_Defense;
+        m_Price = price;
+    }
+
+    
+}
