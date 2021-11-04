40233bbe4d7a57ede9d0307af7d04163716865b8
LukeVanLaarhoven <60391960+LukeVanLaarhoven@users.noreply.github.com>
2021-11-04 14:20:15 +0100

    optimized sliders

--- AllForOne/Assets/Scripts/SpawnedUnit.cs
+++ AllForOne/Assets/Scripts/SpawnedUnit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UnitManager : MonoBehaviour
+public class SpawnedUnit : MonoBehaviour
 {
     private Unit unit;
     private UIManager ui;
--- AllForOne/Assets/Scripts/UIManager.cs
+++ AllForOne/Assets/Scripts/UIManager.cs
@@ -14,13 +14,26 @@ public class UIManager : MonoBehaviour
     public Text strenghtText;
     public Text speedText;
     public Text defenceText;
+    public Text totalAvailablePrice;
+    public Text totalAmountUnit;
+    public Text UnitAmountText;
 
     private int healthValue;
     private int strenghtValue;
     private int speedValue;
     private int defenceValue;
+    private int unitAmount;
 
+    private float totalPrice;
+    private float priceUnit;
+
+    public GameObject unitExample;
     public GameObject unit;
+
+    private void Start()
+    {
+        totalPrice = 100;
+    }
 
     private void Update()
     {
@@ -28,11 +41,28 @@ public class UIManager : MonoBehaviour
         strenghtText.text = GetStrenghtValue().ToString("00");
         speedText.text = GetSpeedValue().ToString("00");
         defenceText.text = GetDefenceValue().ToString("00");
+        totalAvailablePrice.text = totalPrice.ToString("000");
+        totalAmountUnit.text = priceUnit.ToString("000");
+        UnitAmountText.text = unitAmount.ToString("00") + "/10";
+
+        priceUnit = map(GetHealthValue(), 1, 100, 3, 30) + map(GetStrenghtValue(), 1, 100, 3, 30) + map(GetSpeedValue(), 1, 100, 2, 20) + map(GetDefenceValue(), 1, 100, 2, 20);
     }
 
     public void OnClick()
     {
-        Instantiate(unit);
+        if (totalPrice >= priceUnit && unitAmount < 10)
+        {
+            Instantiate(unit);
+
+            totalPrice -= priceUnit;
+
+            unitAmount++;
+        }
+    }
+
+    float map(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
     }
 
     public int GetHealthValue()
